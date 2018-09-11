e4aa196b38f28064470c4c62da9924722ade7a70
Myriam Martins <myriambio@yahoo.com.br>
2018-09-10 21:46:12 -0300

    Primeiro commit

---
+++ src/exemplo/MinhaClasse.java
@@ -0,0 +1,18 @@
+package exemplo;
+
+public class MinhaClasse {
+	
+	private String nome;
+	private String cpf;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf=cpf;
+	}
+} 
